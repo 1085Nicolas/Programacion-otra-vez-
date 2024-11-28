5a93bd0b66c4326d5d6e0edcf6bda89ef35f4039
manuelba1207 <mabastidas@unal.edu.co>
2024-11-27 23:35:13 -0500

    se realizo el primer punto

--- trabajo.py
+++ trabajo.py
@@ -1,1 +1,21 @@
-trabajo
+#primer punto
+def vacas (V,K,X,c):
+    if K > c and X <= 1 :
+        return "0 ya que no se puede producir ni un litro diario"
+    else :
+        vacas_posibles = min (V,c//K)
+        p = vacas_posibles * X * 7
+        return p
+
+
+V = int(input("Cuantas vacas hay: "))
+K = float(input("¿Cuantos metros cuadrados de pasto necesita por cada vaca? : "))
+X = float(input("litros de leche por vaca al dia : "))
+M = float(input("¿Cual es el ancho del corral de las vacas en metros? : "))
+N = float(input("¿Cual es el largo del corral de las vacas en metros? : "))
+
+c = M * N
+
+resultado = vacas (V,K,X,c)
+
+print ("Los litros de leche producidos por semana son : ", resultado)
